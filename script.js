1e1d67df401319df93f3a722444de292239533e2
Вера <94964842+Veramelli@users.noreply.github.com>
2023-04-02 16:41:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,10 @@
 fetch("https://jsonplaceholder.typicode.com/posts")
   .then((response) => response.json())
   .then((data) => {
-    console.log(data);
     let postContent = "";
     for (let object of data) {
       const objectTitle = object.title;
       const objectBody = object.body;
-      /*       console.log(objectBody);
-       */
       postContent += `<div class="post__item">
       <div class="post__title">${objectTitle}</div>
       <div class="post__body">${objectBody}</div>
